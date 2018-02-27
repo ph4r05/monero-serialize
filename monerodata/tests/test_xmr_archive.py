462e11dd864668d5cce52e15f07d27397f6bb6a1
Dusan Klinec <dusan.klinec@gmail.com>
2018-02-27 23:17:00 +0100

    borro test in archive

--- monerodata/tests/test_xmr_archive.py
+++ monerodata/tests/test_xmr_archive.py
@@ -20,6 +20,18 @@ class XmrTypesBaseTest(aiounittest.AsyncTestCase):
 
     def __init__(self, *args, **kwargs):
         super(XmrTypesBaseTest, self).__init__(*args, **kwargs)
+        self.ec_offset = 0
+
+    def setUp(self):
+        self.ec_offset = 0
+
+    def generate_rand_ec_key(self, use_offset=True):
+        offset = 0
+        if use_offset:
+            offset = self.ec_offset
+            self.ec_offset += 1
+
+        return bytearray(range(offset, offset + 32))
 
     async def test_simple_msg(self):
         """
@@ -39,7 +51,26 @@ class XmrTypesBaseTest(aiounittest.AsyncTestCase):
         self.assertEqual(msg.height, msg2.height)
         self.assertEqual(msg, msg2)
 
+    async def test_boro_sig(self):
+        """
+        BoroSig
+        :return:
+        """
+        ee = self.generate_rand_ec_key()
+        s0 = [self.generate_rand_ec_key() for _ in range(64)]
+        s1 = [self.generate_rand_ec_key() for _ in range(64)]
+        msg = xmr.BoroSig(s0=s0, s1=s1, ee=ee)
 
+        writer = x.MemoryReaderWriter()
+        ar1 = x.Archive(writer, True)
+        await ar1.message(msg)
+
+        msg2 = xmr.BoroSig()
+        ar2 = x.Archive(x.MemoryReaderWriter(writer.buffer), False)
+        await ar2.message(msg2)
+
+        self.assertEqual(msg, msg2)
+        
 
 if __name__ == "__main__":
     unittest.main()  # pragma: no cover
