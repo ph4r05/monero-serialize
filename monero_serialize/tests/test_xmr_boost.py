4cd5ece6f98f62d7529a01e1d39293418029c8f1
Dusan Klinec <dusan.klinec@gmail.com>
2018-04-21 10:05:35 +0200

    boost: tx destination test

--- monero_serialize/tests/test_xmr_boost.py
+++ monero_serialize/tests/test_xmr_boost.py
@@ -58,6 +58,33 @@ class XmrBoostTest(aiounittest.AsyncTestCase):
         msg = xmr.CtKey()
         await ar.root_message(msg)
 
+    async def test_destination_entries(self):
+        """
+        Tx destinations, two records, versioning test.
+        :return:
+        """
+        data_hex = b'011673657269616c697a6174696f6e3a3a6172636869766500000001010144000000000120cc000000000000000000000000000000000000000000000000000000000000ee0120aa000000000000000000000000000000000000000000000000000000000000dd01010122012011000000000000000000000000000000000000000000000000000000000000ee012033000000000000000000000000000000000000000000000000000000000000dd00'
+        data_bin = base64.b16decode(data_hex, True)
+        reader = x.MemoryReaderWriter(bytearray(data_bin))
+        ar = xmrb.Archive(reader, False)
+
+        msg = xmr.TxDestinationEntry()
+        await ar.root_message(msg)
+
+        self.assertEqual(msg.amount, 0x44)
+        self.assertEqual(msg.is_subaddress, 1)
+        self.assertEqual(msg.addr.m_spend_public_key, bytearray(b'\xcc' + (b'\x00' * 30) + b'\xee'))
+        self.assertEqual(msg.addr.m_view_public_key, bytearray(b'\xaa' + (b'\x00' * 30) + b'\xdd'))
+
+        msg2 = xmr.TxDestinationEntry()
+        await ar.message(msg2)
+
+        self.assertEqual(msg2.amount, 0x22)
+        self.assertEqual(msg2.is_subaddress, 0)
+        self.assertEqual(msg2.addr.m_spend_public_key, bytearray(b'\x11' + (b'\x00' * 30) + b'\xee'))
+        self.assertEqual(msg2.addr.m_view_public_key, bytearray(b'\x33' + (b'\x00' * 30) + b'\xdd'))
+
+
 
 
 
