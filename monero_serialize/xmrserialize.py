a830232909bd9eb3be88802e3536495479931531
Dusan Klinec <dusan.klinec@gmail.com>
2018-06-24 17:28:46 +0200

    xmr: reader writer - raise EOFError on reading empty buffer

--- monero_serialize/xmrserialize.py
+++ monero_serialize/xmrserialize.py
@@ -417,13 +417,17 @@ class MessageType(XmrType):
 
 class MemoryReaderWriter:
 
-    def __init__(self, buffer=None):
+    def __init__(self, buffer=None, read_empty=False, **kwargs):
         self.buffer = buffer if buffer else []
         self.nread = 0
         self.nwritten = 0
+        self.read_empty = read_empty
 
     async def areadinto(self, buf):
         ln = len(buf)
+        if not self.read_empty and len(self.buffer) == 0:
+            raise EOFError
+
         nread = min(ln, len(self.buffer))
         for idx in range(nread):
             buf[idx] = self.buffer.pop(0)
