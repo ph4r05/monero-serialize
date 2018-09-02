9709084a44b909f5632b634ab0c37c1bae0e7b1a
Dusan Klinec <dusan.klinec@gmail.com>
2018-09-02 15:20:17 +0200

    xmr: cn archive cleanup, tracker fix

--- monero_serialize/xmrserialize.py
+++ monero_serialize/xmrserialize.py
@@ -36,6 +36,7 @@ required:
 import logging
 import sys
 
+from . import helpers
 from .protobuf import const, load_uvarint, dump_uvarint
 from .core.readwriter import MemoryReaderWriter
 from .core.base_types import *
@@ -60,7 +61,6 @@ def import_def(module, name):
     return r
 
 
-
 class Archive(object):
     """
     Archive object for object binary serialization / deserialization.
@@ -77,6 +77,7 @@ class Archive(object):
     def __init__(self, iobj, writing=True, **kwargs):
         self.writing = writing
         self.iobj = iobj
+        self.tracker = helpers.Tracker()
 
     async def tag(self, tag):
         """
@@ -308,14 +309,18 @@ class Archive(object):
         :return:
         """
         elem_type = msg_type if msg_type is not None else msg.__class__
+        msg = elem_type() if msg is None else msg
         if hasattr(elem_type, "serialize_archive"):
-            msg = elem_type() if msg is None else msg
             return await msg.serialize_archive(self)
 
-        if self.writing:
-            return await self._dump_message(self.iobj, msg, msg_type=msg_type)
-        else:
-            return await self._load_message(self.iobj, msg_type, msg=msg)
+        mtype = msg.__class__ if msg_type is None else msg_type
+        fields = mtype.f_specs()
+        if hasattr(mtype, "serialize_archive"):
+            raise ValueError("Cannot directly load, has to use archive with %s" % mtype)
+
+        for field in fields:
+            await self.message_field(msg, field)
+        return msg
 
     async def message_field(self, msg, field, fvalue=None):
         """
@@ -325,10 +330,17 @@ class Archive(object):
         :param fvalue: explicit value for dump
         :return:
         """
-        if self.writing:
-            await self._dump_message_field(self.iobj, msg, field, fvalue=fvalue)
-        else:
-            await self._load_message_field(self.iobj, msg, field)
+        fname, ftype, params = field[0], field[1], field[2:]
+        try:
+            self.tracker.push_field(fname)
+            if self.writing:
+                await self._dump_message_field(self.iobj, msg, field, fvalue=fvalue)
+            else:
+                await self._load_message_field(self.iobj, msg, field)
+            self.tracker.pop()
+
+        except Exception as e:
+            raise helpers.ArchiveException(e, tracker=self.tracker) from e
 
     async def message_fields(self, msg, fields):
         """
@@ -499,10 +511,15 @@ class Archive(object):
 
         elem_type = container_elem_type(container_type, params)
 
-        for elem in container:
-            await self.dump_field(
-                writer, elem, elem_type, params[1:] if params else None
-            )
+        for idx, elem in enumerate(container):
+            try:
+                self.tracker.push_index(idx)
+                await self.dump_field(
+                    writer, elem, elem_type, params[1:] if params else None
+                )
+                self.tracker.pop()
+            except Exception as e:
+                raise helpers.ArchiveException(e, tracker=self.tracker) from e
 
     async def _load_container(
             self, reader, container_type, params=None, container=None
@@ -529,12 +546,18 @@ class Archive(object):
         elem_type = container_elem_type(container_type, params)
         res = container if container else []
         for i in range(c_len):
-            fvalue = await self.load_field(
-                reader,
-                elem_type,
-                params[1:] if params else None,
-                eref(res, i) if container else None,
-            )
+            try:
+                self.tracker.push_index(i)
+                fvalue = await self.load_field(
+                    reader,
+                    elem_type,
+                    params[1:] if params else None,
+                    eref(res, i) if container else None,
+                )
+                self.tracker.pop()
+            except Exception as e:
+                raise helpers.ArchiveException(e, tracker=self.tracker) from e
+
             if not container:
                 res.append(fvalue)
         return res
@@ -559,9 +582,14 @@ class Archive(object):
         if elem_fields is None:
             elem_fields = elem_type.f_specs()
         for idx, elem in enumerate(elem):
-            await self.dump_field(
-                writer, elem, elem_fields[idx], params[1:] if params else None
-            )
+            try:
+                self.tracker.push_index(idx)
+                await self.dump_field(
+                    writer, elem, elem_fields[idx], params[1:] if params else None
+                )
+                self.tracker.pop()
+            except Exception as e:
+                raise helpers.ArchiveException(e, tracker=self.tracker) from e
 
     async def _load_tuple(self, reader, elem_type, params=None, elem=None):
         """
@@ -587,12 +615,18 @@ class Archive(object):
 
         res = elem if elem else []
         for i in range(c_len):
-            fvalue = await self.load_field(
-                reader,
-                elem_fields[i],
-                params[1:] if params else None,
-                eref(res, i) if elem else None,
-            )
+            try:
+                self.tracker.push_index(i)
+                fvalue = await self.load_field(
+                    reader,
+                    elem_fields[i],
+                    params[1:] if params else None,
+                    eref(res, i) if elem else None,
+                )
+                self.tracker.pop()
+            except Exception as e:
+                raise helpers.ArchiveException(e, tracker=self.tracker) from e
+
             if not elem:
                 res.append(fvalue)
         return res
@@ -623,45 +657,6 @@ class Archive(object):
         """
         fname, ftype, params = field[0], field[1], field[2:]
         await self.load_field(reader, ftype, params, eref(msg, fname))
-
-    async def _dump_message(self, writer, msg, msg_type=None):
-        """
-        Dumps message to the writer.
-
-        :param writer:
-        :param msg:
-        :param msg_type:
-        :return:
-        """
-        mtype = msg.__class__ if msg_type is None else msg_type
-        fields = mtype.f_specs()
-        if hasattr(mtype, "serialize_archive"):
-            raise ValueError("Cannot directly load, has to use archive with %s" % mtype)
-
-        for field in fields:
-            await self._dump_message_field(writer, msg=msg, field=field)
-
-    async def _load_message(self, reader, msg_type, msg=None):
-        """
-        Loads message if the given type from the reader.
-        Supports reading directly to existing message.
-
-        :param reader:
-        :param msg_type:
-        :param msg:
-        :return:
-        """
-        msg = msg_type() if msg is None else msg
-        fields = msg_type.f_specs() if msg_type else msg.__class__.f_specs()
-        if hasattr(msg_type, "serialize_archive"):
-            raise ValueError(
-                "Cannot directly load, has to use archive with %s" % msg_type
-            )
-
-        for field in fields:
-            await self._load_message_field(reader, msg, field)
-
-        return msg
 
     async def _dump_variant(self, writer, elem, elem_type=None, params=None):
         """
@@ -718,283 +713,6 @@ class Archive(object):
                     elem.set_variant(field[0], fvalue)
                 return elem if is_wrapped else fvalue
         raise ValueError("Unknown tag: %s" % tag)
-
-    # async def prepare_container(self, size, container, elem_type=None):
-    #     """
-    #     Prepares container for serialization
-    #     :param size:
-    #     :param container:
-    #     :return:
-    #     """
-    #     if not self.writing:
-    #         if container is None:
-    #             return gen_elem_array(size, elem_type)
-    #
-    #         fvalue = get_elem(container)
-    #         if fvalue is None:
-    #             fvalue = []
-    #         fvalue += gen_elem_array(max(0, size - len(fvalue)), elem_type)
-    #         set_elem(container, fvalue)
-    #         return fvalue
-    #
-    # async def prepare_message(self, msg, msg_type):
-    #     """
-    #     Prepares message for serialization
-    #     :param msg:
-    #     :param msg_type:
-    #     :return:
-    #     """
-    #     if self.writing:
-    #         return
-    #     return set_elem(msg, msg_type())
-    #
-    # async def uvarint(self, elem):
-    #     """
-    #     Uvarint
-    #     :param elem:
-    #     :return:
-    #     """
-    #     if self.writing:
-    #         return await dump_uvarint(self.iobj, elem)
-    #     else:
-    #         return await load_uvarint(self.iobj)
-    #
-    # async def uint(self, elem, elem_type, params=None):
-    #     """
-    #     Fixed size int
-    #     :param elem:
-    #     :param elem_type:
-    #     :param params:
-    #     :return:
-    #     """
-    #     if self.writing:
-    #         return await dump_uint(self.iobj, elem, elem_type.WIDTH)
-    #     else:
-    #         return await load_uint(self.iobj, elem_type.WIDTH)
-    #
-    # async def unicode_type(self, elem):
-    #     """
-    #     Unicode type
-    #     :param elem:
-    #     :return:
-    #     """
-    #     if self.writing:
-    #         return await dump_unicode(self.iobj, elem)
-    #     else:
-    #         return await load_unicode(self.iobj)
-    #
-    # async def blob(self, elem=None, elem_type=None, params=None):
-    #     """
-    #     Loads/dumps blob
-    #     :return:
-    #     """
-    #     elem_type = elem_type if elem_type else elem.__class__
-    #     if hasattr(elem_type, 'serialize_archive'):
-    #         elem = elem_type() if elem is None else elem
-    #         return await elem.serialize_archive(self, elem=elem, elem_type=elem_type, params=params)
-    #
-    #     if self.writing:
-    #         return await dump_blob(self.iobj, elem=elem, elem_type=elem_type, params=params)
-    #     else:
-    #         return await load_blob(self.iobj, elem_type=elem_type, params=params, elem=elem)
-    #
-    # async def container(self, container=None, container_type=None, params=None):
-    #     """
-    #     Loads/dumps container
-    #     :return:
-    #     """
-    #     if hasattr(container_type, 'serialize_archive'):
-    #         container = container_type() if container is None else container
-    #         return await container.serialize_archive(self, elem=container, elem_type=container_type, params=params)
-    #
-    #     if self.writing:
-    #         return await dump_container(self.iobj, container, container_type, params,
-    #                                     field_archiver=self.dump_field)
-    #     else:
-    #         return await load_container(self.iobj, container_type, params=params, container=container,
-    #                                     field_archiver=self.load_field)
-    #
-    # async def container_size(self, container_len=None, container_type=None, params=None):
-    #     """
-    #     Container size
-    #     :param container_len:
-    #     :param container_type:
-    #     :param params:
-    #     :return:
-    #     """
-    #     if hasattr(container_type, 'serialize_archive'):
-    #         raise ValueError('not supported')
-    #
-    #     if self.writing:
-    #         return await dump_container_size(self.iobj, container_len, container_type, params)
-    #     else:
-    #         raise ValueError('Not supported')
-    #
-    # async def container_val(self, elem, container_type, params=None):
-    #     """
-    #     Single cont value
-    #     :param elem:
-    #     :param container_type:
-    #     :param params:
-    #     :param field_archiver:
-    #     :return:
-    #     """
-    #     if hasattr(container_type, 'serialize_archive'):
-    #         raise ValueError('not supported')
-    #     if self.writing:
-    #         return await dump_container_val(self.iobj, elem, container_type, params)
-    #     else:
-    #         raise ValueError('Not supported')
-    #
-    # async def tuple(self, elem=None, elem_type=None, params=None):
-    #     """
-    #     Loads/dumps tuple
-    #     :return:
-    #     """
-    #     if hasattr(elem_type, 'serialize_archive'):
-    #         container = elem_type() if elem is None else elem
-    #         return await container.serialize_archive(self, elem=elem, elem_type=elem_type, params=params)
-    #
-    #     if self.writing:
-    #         return await dump_tuple(self.iobj, elem, elem_type, params,
-    #                                 field_archiver=self.dump_field)
-    #     else:
-    #         return await load_tuple(self.iobj, elem_type, params=params, elem=elem,
-    #                                 field_archiver=self.load_field)
-    #
-    # async def variant(self, elem=None, elem_type=None, params=None):
-    #     """
-    #     Loads/dumps variant type
-    #     :param elem:
-    #     :param elem_type:
-    #     :param params:
-    #     :return:
-    #     """
-    #     elem_type = elem_type if elem_type else elem.__class__
-    #     if hasattr(elem_type, 'serialize_archive'):
-    #         elem = elem_type() if elem is None else elem
-    #         return await elem.serialize_archive(self, elem=elem, elem_type=elem_type, params=params)
-    #
-    #     if self.writing:
-    #         return await dump_variant(self.iobj, elem=elem,
-    #                                   elem_type=elem_type if elem_type else elem.__class__,
-    #                                   params=params, field_archiver=self.dump_field)
-    #     else:
-    #         return await load_variant(self.iobj, elem_type=elem_type if elem_type else elem.__class__,
-    #                                   params=params, elem=elem, field_archiver=self.load_field)
-    #
-    # async def message(self, msg, msg_type=None):
-    #     """
-    #     Loads/dumps message
-    #     :param msg:
-    #     :param msg_type:
-    #     :return:
-    #     """
-    #     elem_type = msg_type if msg_type is not None else msg.__class__
-    #     if hasattr(elem_type, 'serialize_archive'):
-    #         msg = elem_type() if msg is None else msg
-    #         return await msg.serialize_archive(self)
-    #
-    #     if self.writing:
-    #         return await dump_message(self.iobj, msg, msg_type=msg_type, field_archiver=self.dump_field)
-    #     else:
-    #         return await load_message(self.iobj, msg_type, msg=msg, field_archiver=self.load_field)
-    #
-    # async def message_field(self, msg, field, fvalue=None):
-    #     """
-    #     Dumps/Loads message field
-    #     :param msg:
-    #     :param field:
-    #     :param fvalue: explicit value for dump
-    #     :return:
-    #     """
-    #     if self.writing:
-    #         await dump_message_field(self.iobj, msg, field, fvalue=fvalue, field_archiver=self.dump_field)
-    #     else:
-    #         await load_message_field(self.iobj, msg, field, field_archiver=self.load_field)
-    #
-    # async def message_fields(self, msg, fields):
-    #     """
-    #     Load/dump individual message fields
-    #     :param msg:
-    #     :param fields:
-    #     :param field_archiver:
-    #     :return:
-    #     """
-    #     for field in fields:
-    #         await self.message_field(msg, field)
-    #     return msg
-    #
-    # async def rfield(self, elem=None, elem_type=None, params=None):
-    #     """
-    #     Loads/Dumps message field
-    #     :param elem:
-    #     :param elem_type:
-    #     :param params:
-    #     :return:
-    #     """
-    #     if self.writing:
-    #         return await dump_field(self.iobj, elem=elem,
-    #                                 elem_type=elem_type if elem_type else elem.__class__,
-    #                                 params=params)
-    #     else:
-    #         return await load_field(self.iobj,
-    #                                 elem_type=elem_type if elem_type else elem.__class__,
-    #                                 params=params,
-    #                                 elem=elem)
-    #
-    # async def field(self, elem=None, elem_type=None, params=None):
-    #     """
-    #     Archive field
-    #     :param elem:
-    #     :param elem_type:
-    #     :param params:
-    #     :return:
-    #     """
-    #     elem_type = elem_type if elem_type else elem.__class__
-    #     fvalue = None
-    #     if issubclass(elem_type, UVarintType):
-    #         fvalue = await self.uvarint(get_elem(elem))
-    #
-    #     elif issubclass(elem_type, IntType):
-    #         fvalue = await self.uint(elem=get_elem(elem), elem_type=elem_type, params=params)
-    #
-    #     elif issubclass(elem_type, BlobType):
-    #         fvalue = await self.blob(elem=get_elem(elem), elem_type=elem_type, params=params)
-    #
-    #     elif issubclass(elem_type, UnicodeType):
-    #         fvalue = await self.unicode_type(get_elem(elem))
-    #
-    #     elif issubclass(elem_type, VariantType):
-    #         fvalue = await self.variant(elem=get_elem(elem), elem_type=elem_type, params=params)
-    #
-    #     elif issubclass(elem_type, ContainerType):  # container ~ simple list
-    #         fvalue = await self.container(container=get_elem(elem), container_type=elem_type, params=params)
-    #
-    #     elif issubclass(elem_type, TupleType):  # tuple ~ simple list
-    #         fvalue = await self.tuple(elem=get_elem(elem), elem_type=elem_type, params=params)
-    #
-    #     elif issubclass(elem_type, MessageType):
-    #         fvalue = await self.message(get_elem(elem), msg_type=elem_type)
-    #
-    #     else:
-    #         raise TypeError
-    #
-    #     return fvalue if self.writing else set_elem(elem, fvalue)
-    #
-    # async def dump_field(self, writer, elem, elem_type, params=None):
-    #     assert self.iobj == writer
-    #     return await self.field(elem=elem, elem_type=elem_type, params=params)
-    #
-    # async def load_field(self, reader, elem_type, params=None, elem=None):
-    #     assert self.iobj == reader
-    #     return await self.field(elem=elem, elem_type=elem_type, params=params)
-    #
-    # async def root(self):
-    #     """
-    #     Root level archive init
-    #     :return:
-    #     """
 
 
 async def dump_blob(writer, elem, elem_type, params=None):
